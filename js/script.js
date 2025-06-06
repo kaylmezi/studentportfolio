588bc18f11110deb3d655647fbd61ed0d2e8dba1
kaylmezi <kyllemizziej@gmail.com>
2025-06-06 12:51:59 +0800

    changes

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@
     const navLinks = document.getElementById('nav-links');
  
     hamburger.addEventListener('click', () => {
-      navLinks.classList.toggle('show');
+      navLinks.classList.toggle('active');
     });
