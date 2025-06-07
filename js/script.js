0e392fb148100eeb74808d1ecc6641815b8f9da6
kaylmezi <kyllemizziej@gmail.com>
2025-06-07 08:55:28 +0800

    fix

--- js/script.js
+++ js/script.js
@@ -3,28 +3,25 @@ const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active');
 
   document.body.style.overflow = navLinks.classList.contains('active') ? "hidden" : "auto";
 
-  if(navLinks.classList.contains('active')) {
-    hamburger.innerHTML = '&times;';
-  } else {
-    hamburger.innerHTML = '&#9776;';
-  }
   });
 
 document.addEventListener('click', (e) => {
-  if(navLinks.classList.contains('active')) {
-    if(!navLinks.contains(e.target) && e.target !== hamburger) {
+  if(navLinks.classList.contains('active') && 
+  !navLinks.contains(e.target) && !hamburger.contains(e.target)) {
       navLinks.classList.remove('active');
+      hamburger.classList.remove('active');
       document.body.style.overflow = 'auto';
     }
-  }
 });
 
 document.querySelectorAll('.nav-links a').forEach(link => {
   link.addEventListener('click', () => {
     navLinks.classList.remove('active');
+    hamburger.classList.remove('active');
     document.body.style.overflow = 'auto';
 });
 });
